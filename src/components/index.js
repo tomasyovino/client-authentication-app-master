94e73025d947996f034348d6bafbf89c780ac8b1
tomasyovino <tomasyovino@gmail.com>
2023-02-01 13:29:13 -0300

    Add Spinner component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 export { default as PersonalInfo } from "./PersonalInfo";
 export { default as ChangePersonalInfo } from "./ChangePersonalInfo";
-export { default as Form } from "./Form";
+export { default as RegisterForm } from "./RegisterForm";
+export { default as LoginForm } from "./LoginForm";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Layout } from "./Layout";
+export { default as Spinner } from "./Spinner";
