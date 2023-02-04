ca257f30ef5a663bc01800ce77c8c1094ea5b164
tomasyovino <tomasyovino@gmail.com>
2023-02-04 19:24:46 -0300

    Add tailwind & headlessui modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
